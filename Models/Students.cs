491d5b3bd81555cb8b49af622aa15cf4868b2395
Thomas Van Poucke <thomasvanpoucke@outlook.com>
2021-01-01 14:25:54 +0100

    whole blazor website

---
+++ Models/Students.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_Blazor101.Models
+{
+    public class Student
+    {
+        //properties
+        public int Nr { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+
+    }
+}
---
+++ Models/TodoItems.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_Blazor101.Models
+{
+    public class TodoItem
+    {
+        //properties
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
